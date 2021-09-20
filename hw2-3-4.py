85d3fb699da7ab269fc85c30ad5b742be9e63577
p22cobrien <p22cobrien@fairifeldprep.org>
2021-09-20 17:02:41 -0400

    add hw2-3-4.py

---
+++ hw2-3-4.py
@@ -0,0 +1,14 @@
+# Author: CMOB 9/20/2021
+
+origin_pop = 331832432
+days = 367
+pop_per_day = 13824
+
+total_pop = origin_pop + (days * pop_per_day)
+
+print(total_pop)
+
+#thought process: Septmeber 23rd 2020 - US pop = 331832432
+# estimate pop per day 13824 ( one every 25 seconds)
+#total days since spetember 23rd 2020 - 367 leap year
+#origin pop + (days * pop per day)
