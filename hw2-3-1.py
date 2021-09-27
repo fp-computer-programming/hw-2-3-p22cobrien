e0b0146b1a9cac604844c9ca23c7f5fa90c4ee6d
p22cobrien <p22cobrien@fairfieldprep.org>
2021-09-27 09:54:25 -0400

    correction

--- hw2-3-1.py
+++ hw2-3-1.py
@@ -1,9 +1,7 @@
 # Author: CMOB 9/20/2021
 
-print("```")
 print("PPPPP   Y   Y   TTTTT   H   H   OOOOO   N   N")
 print("P   P    Y Y      T     H   H   O   O   NN  N")
 print("PPPPP     Y       T     HHHHH   O   O   N N N")
 print("P         Y       T     H   H   O   O   N  NN")
 print("P         Y       T     H   H   OOOOO   N   N")
-print("```")
